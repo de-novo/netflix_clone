98c9fe7bbd947414a9961353f7bc4e3a6448c37e
denovo <vo0v0000@naver.com>
2022-01-06 05:14:33 +0900

    json 수정

--- netflix/server/config/index.js
+++ netflix/server/config/index.js
@@ -2,10 +2,10 @@ import dotenv from "dotenv";
 
 process.env.NODE_ENV = process.env.NODE_ENV || "development";
 
-const envFound = dotenv.config();
-if (envFound.error) {
-    throw new Error("Couldn't find .env file");
-}
+// const envFound = dotenv.config();
+// if (envFound.error) {
+//     throw new Error("Couldn't find .env file");
+// }
 export default {
     port: process.env.PORT || 8080,
     // mongoDB URL
